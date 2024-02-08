19b08d0cfb8ca82d6e332fbd3850db5775f74ea7
purnima-28 <146444855+purnima-28@users.noreply.github.com>
2024-02-08 17:12:17 +0530

    final project

---
+++ index1.js
@@ -0,0 +1,28 @@
+let button=document.querySelector("#btn")
+let save=document.querySelector("#save")
+let todo=document.querySelector("#todo")
+let popup=document.querySelector("#pop")
+todo.addEventListener("click",() => {
+    popup.classList.remove("d-none")
+
+})
+save.addEventListener("click",() => {
+    popup.classList.add("d-none")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
